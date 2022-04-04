9bd3b21e1000a2ec08b9ec2578c16c6207985dae
Kevin C. Tofel <kevin@kctofel.com>
2022-04-04 16:33:24 -0400

    Even numbers complete

---
+++ EvenNumbers.js
@@ -0,0 +1,6 @@
+// Log all even numbers from 1 to 99, inclusive, to the console, with
+// each number on a separate line.
+
+for (let num = 1; num <= 99; num += 1) {
+  if (num % 2 === 0) console.log(num);
+}
