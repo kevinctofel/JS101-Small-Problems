f2a5f9c785b3d163f26a7250a0783c0b074244f2
Kevin C. Tofel <kevin@kctofel.com>
2022-04-04 10:26:13 -0400

    OddNumbers complete

--- OddNumbers.js
+++ OddNumbers.js
@@ -1,6 +1,6 @@
-// Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+// Log all odd numbers from 1 to 99, inclusive, to the console, with
+// each number on a separate line.
 
-for (let i = 1; i <= 99; i++) {
-  console.log(i);
+for (let num = 1; num <= 99; num += 2) {
+  console.log(num);
 }
-
