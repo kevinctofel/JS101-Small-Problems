2f59c23dee549dc3beff914499c0713403de9c77
Kevin C. Tofel <kevin@kctofel.com>
2022-09-04 17:10:01 -0400

    Initial

---
+++ CleanUpTheWords.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-len */
+// Given a string that consists of some words and an assortment of non-alphabetic characters, write a function that returns that string with all of the non-alphabetic characters replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have one space in the result (i.e., the result string should never have consecutive spaces).
+
+/*
+P: input is a string, output is also a string with non-alphanumeric characters removed / replaced by spaces. Multiple spaces are to be one space
+E:
+D: May need to change string to array and back to string (join)
+A:  Convert string to array of individual characters
+    Iterate through characters and save alpha-numerics in new string. Add space for non-alpha (only one if multiple non-alphas)
+C:
+
+*/
+
+const cleanUp = (origString) => {
+  let re = new RegExp(/[^A-Za-z0-9]/g); // RegEx for alphanumerics
+  let output = origString.replace(re, " ");
+  let result = '';
+  console.log(output);
+  let outputArray = output.split('');
+  for (let index = 0; index < outputArray.length; index++) {
+    if (outputArray[index] === ' ') {
+      while (outputArray[index + 1] === '') {
+        index++;
+      }
+      result += '';
+    }
+    result += '';
+  }
+  return result;
+};
+
+
+
+cleanUp("---what's my +*& line?");    // " what s my line "
